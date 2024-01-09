63d78a17bc17735c610fcdee992cc9f9b9e5dd30
m4cey <h4mmer7ime@gmail.com>
2024-01-09 14:48:47 +0100

    fix

--- .local/share/dwm/autostart.sh
+++ .local/share/dwm/autostart.sh
@@ -1,14 +1,13 @@
 #! /bin/zsh
 
-remaps
 pkill dunst
 dunst &
 pkill bar.sh
 bar.sh &
 playerctld daemon
-# xinput --set-prop "TPPS/2 IBM TrackPoint" "Coordinate Transformation Matrix" 6 0 0 0 6 0 0 0 1
 xset r rate 300 50 &	# Speed xrate up
 pkill xautolock
 xautolock -time 20 -locker "slock" -notifier "notify-send 'sleeping in 10 seconds!'" -notify 10 &
 rfkill block bluetooth
+pkill picom
 picom &
